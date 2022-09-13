b1eecdd3d3867fea39c1f798ed880988adf4ce6e
withkeygames <withkeygames@gmail.com>
2022-09-13 15:33:05 +0300

    Solved

--- src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
+++ src/main/java/com/epam/autotasks/collections/IntStringCappedMap.java
@@ -1,20 +1,21 @@
 package com.epam.autotasks.collections;
 
-import java.util.AbstractMap;
-import java.util.AbstractSet;
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
+
 
 class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     private final long capacity;
 
+    private Map<Integer, String> map ;
+
     public IntStringCappedMap(final long capacity) {
         this.capacity = capacity;
+        map = new LinkedHashMap<>((int)capacity);
+    }
+
+    public static <K, V> Entry<K, V> getFirst(Map<K, V> map) {
+        if (map.isEmpty()) return null; return map.entrySet().iterator().next();
     }
 
     public long getCapacity() {
@@ -27,16 +28,20 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
             @Override
             public Iterator<Entry<Integer, String>> iterator() {
                 return new Iterator<>() {
+                    Iterator<Entry<Integer, String>> value = map.entrySet().iterator();
+
                     @Override
                     public boolean hasNext() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        return value.hasNext();
                     }
 
                     @Override
                     public Entry<Integer, String> next() {
-                        //implement this method
-                        throw new UnsupportedOperationException();
+                        if (value.hasNext()) {
+                            return value.next();
+                        } else {
+                            throw new NoSuchElementException();
+                        }
                     }
                 };
             }
@@ -50,26 +55,43 @@ class IntStringCappedMap extends AbstractMap<Integer, String> {
 
     @Override
     public String get(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.get(key);
     }
 
+    private long totalSizeOfValues() {
+        long total_size = 0;
+        for (Map.Entry<Integer, String> entry: map.entrySet()) {
+            total_size += entry.getValue().length();
+        }
+        return total_size;
+    }
     @Override
     public String put(final Integer key, final String value) {
-        //implement this method
-        throw new UnsupportedOperationException();
+        if (value.length() > capacity) throw new IllegalArgumentException();
+        String previousValue = null;
+        if (map.containsKey(key)) {
+            previousValue = map.remove(key);
+        }
+        while (totalSizeOfValues() + value.length() > capacity) {
+            map.remove(getFirst(map).getKey());
+        }
+
+        String str = map.put(key, value);
+        if (previousValue != null) return previousValue;
+        return str;
+
     }
+
 
     @Override
     public String remove(final Object key) {
-        //implement this method
-        throw new UnsupportedOperationException();
+         return map.remove(key);
     }
 
     @Override
     public int size() {
-        //implement this method
-        throw new UnsupportedOperationException();
+        return map.size();
+
     }
 
 }
